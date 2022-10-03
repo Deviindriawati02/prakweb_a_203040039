960b6e9e964413f52f9f8f79ce994b7fa8ea4570
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-10-03 23:10:30 +0700

    menyelesaikan video 6

--- oop1/inheritance-problem.php
+++ oop1/inheritance-problem.php
@@ -5,39 +5,60 @@ class Produk
   public $judul,
     $penulis,
     $penerbit,
-    $harga;
+    $harga,
+    $jmlHalaman,
+    $waktuMain,
+    $tipe;
 
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe)
   {
+    // echo "Hello World!";
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
     $this->harga = $harga;
+    $this->jmlHalaman = $jmlHalaman;
+    $this->waktuMain = $waktuMain;
+    $this->tipe = $tipe;
   }
 
   public function getLabel()
   {
     return "$this->penulis, $this->penerbit";
   }
-}
 
-class CetakInfoProduk {
-    public function cetak($produk ) {
-        $str = "{$produk-> judul}| {$produk->getLabel()}, Shonen Jump (Rp.{$produk->harga})";
-        return $str;
+  public function getInfoLengkap()
+  {
+    // Komik : Naruto | Masashi Kishimoto, Shonen Jump, (Rp. 30000) - 100 Halaman.
+    // $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()}, (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()}, (Rp. {$this->harga})";
+
+    if ($this->tipe == "Komik") {
+      $str .= " - {$this->jmlHalaman} Halaman.";
+    } else if ($this->tipe == "Game") {
+      $str .= " - {$this->waktuMain} Jam.";
     }
+
+    return $str;
+  }
 }
 
+class CetakInfoProduk
+{
+  public function cetak(Produk $produk)
+  {
+    // $str = "Naruto | Masashi Kishimoto, Shonen Jump, (Rp. 30000)";
+    $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+    return $str;
+  }
+}
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony computer", 250000);
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump, (Rp. 30000) - 100 Halaman.
+// Game : Uncharted | Neil Druckmann, Sony Computer, (Rp. 250000) - 50 Jam.
 
-
-echo "Komik : " . $produk1->getLabel();
+echo $produk1->getInfoLengkap();
 echo "<br>";
-echo "Game : " . $produk2->getLabel();
-echo "<br>";
-
-$infoProduk1 = new CetakInfoProduk();
-echo $infoProduk1->cetak($produk1);
+echo $produk2->getInfoLengkap();
