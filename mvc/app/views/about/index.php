8c92152cf3f46a4711033bcde52efd28d08287fb
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-10-10 00:27:46 +0700

    menyelesaikan video 6 assets

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/dep.jpeg" alt="Devi indriawati" width="200" class="rounded-circle shadow">
 
-    <h1>About me</h1>
-    <p>Halo, nama saya <?= $data['nama']; ?> umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    <p>Halo, nama saya <?= $data['nama']; ?> umur saya <?= $data['umur']; ?> tahun, saya adalah seorang
+     <?= $data['pekerjaan']; ?>.</p>
+     </div>
