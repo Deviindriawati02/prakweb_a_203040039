ddb099a5ca38bfe88a96deee809e037cbb5f57ca
Deviindriawati02 <203040039@mail.unpas.ac.id>
2022-10-10 10:18:44 +0700

    menyelesaikan video 8 Database Wrapper

--- mvc/app/controllers/Mahasiswa.php
+++ mvc/app/controllers/Mahasiswa.php
@@ -9,4 +9,12 @@ class Mahasiswa extends Controller {
         $this->view('Mahasiswa/index', $data);
         $this->view('templates/footer');
     }
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasisa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header' , $data);
+        $this->view('Mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
 }
